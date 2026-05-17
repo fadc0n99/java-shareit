49011f03d8f5df45b0f47537dade2880ecc6d2d0
fadcon <fad.con0909@gmail.com>
2026-05-17 10:59:26 +0400

    codestyle

--- server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/request/ItemRequestServiceImplTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
